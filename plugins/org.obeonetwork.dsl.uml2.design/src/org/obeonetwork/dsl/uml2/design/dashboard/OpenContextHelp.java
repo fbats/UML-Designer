fd1cf41a7696abd1c67d8a341b16c1943afee0dc
Frederic Bats <frederic.bats@obeo.fr>
2016-03-22 18:25:19 +0100

    Implement new dashboard based on amalgam.

---
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/dashboard/OpenContextHelp.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.uml2.design.dashboard;
+
+import org.eclipse.amalgam.explorer.activity.ui.api.hyperlinkadapter.BlankHyperLinkAdapter;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.events.IHyperlinkListener;
+
+public class OpenContextHelp extends BlankHyperLinkAdapter implements IHyperlinkListener {
+	private final String contextId;
+
+	public OpenContextHelp(String contextId){
+		// Context ids are defined in the html/contexts.xml file in
+		// org.obeonetwork.dsl.uml2.design.doc project.
+		this.contextId =  "org.obeonetwork.dsl.uml2.design.doc." + contextId;//$NON-NLS-1$
+	}
+
+	@Override
+	protected void linkPressed(HyperlinkEvent event, EObject project, Session session) {
+		openContextHelp();
+	}
+
+	private void openContextHelp() {
+		if (PlatformUI.getWorkbench() != null && PlatformUI.getWorkbench().getActiveWorkbenchWindow() != null
+				&& PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell() != null) {
+			PlatformUI.getWorkbench().getHelpSystem()
+			.setHelp(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), contextId);
+			PlatformUI.getWorkbench().getHelpSystem().displayDynamicHelp();
+		}
+	}
+}
