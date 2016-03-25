87da2b2f70319322692c4549875b172d26688118
Frederic Bats <frederic.bats@obeo.fr>
2016-03-25 17:03:04 +0100

    enable open dashboard button

---
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/internal/testers/UMLViewpointEnablementPropertyTester.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.uml2.design.internal.testers;
+
+import java.util.Collection;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+
+/**
+ * Tester to check viewpoints enablement.
+ *
+ * @author Melanie Bats <a href="mailto:melanie.bats@obeo.fr">melanie.bats@obeo.fr</a>
+ */
+public class UMLViewpointEnablementPropertyTester extends PropertyTester {
+	private final String vpCapture = "Capture"; //$NON-NLS-1$
+
+	private final String vpDesgin = "Desgin"; //$NON-NLS-1$
+
+	private final String vpReview = "Review"; //$NON-NLS-1$
+
+	private final String vpExtend = "Extend"; //$NON-NLS-1$
+
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		final Collection<Session> sessions = SessionManager.INSTANCE.getSessions();
+		for (final Session session : sessions) {
+			// Check if the dashboard viewpoint is active for the current
+			// session
+			if (session != null) {
+				for (final Viewpoint vp : session.getSelectedViewpoints(false)) {
+					if (vp.getName().contains(vpCapture) || vp.getName().contains(vpDesgin)
+							|| vp.getName().contains(vpReview) || vp.getName().contains(vpExtend)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
