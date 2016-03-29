90328632c47022df6f8738d32dce37106ced8314
Frederic Bats <frederic.bats@obeo.fr>
2016-03-29 09:57:27 +0200

    Sequence diagram creation

--- plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/dashboard/CreateSequenceDiagram.java
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/dashboard/CreateSequenceDiagram.java
@@ -11,10 +11,51 @@
 package org.obeonetwork.dsl.uml2.design.dashboard;
 
 import org.eclipse.amalgam.explorer.activity.ui.api.hyperlinkadapter.NewDiagramHyperLinkAdapter;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.command.AddCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.uml2.uml.Interaction;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.UMLFactory;
+import org.obeonetwork.dsl.uml2.design.internal.services.ElementServices;
 
 public class CreateSequenceDiagram extends NewDiagramHyperLinkAdapter {
+	private static final String SEQUENCE_DIAGRAM = "Sequence Diagram"; //$NON-NLS-1$
+
+	@Override
+	protected boolean createDiagram(EObject project, Session session) {
+		if (!(project instanceof Package)) {
+			return false;
+		}
+
+		final Interaction interaction = createInteraction(project);
+		final TransactionalEditingDomain domain = session.getTransactionalEditingDomain();
+		final CommandStack commandStack = domain.getCommandStack();
+
+		final Command command = AddCommand.create(domain, project, null, interaction);
+		commandStack.execute(command);
+		return super.createDiagram(interaction, session);
+	}
+
+	/**
+	 * Create interaction a new interaction in package.
+	 *
+	 * @param pkg
+	 *            Package containing new interaction.
+	 * @return Interaction
+	 */
+	public Interaction createInteraction(EObject pkg) {
+		final UMLFactory factory = UMLFactory.eINSTANCE;
+		final Interaction interaction = factory.createInteraction();
+		interaction.setName(ElementServices.INSTANCE.getNewInteractionName(pkg));
+		return interaction;
+	}
+
 	@Override
 	public String getRepresentationName() {
-		return "Sequence Diagram"; //$NON-NLS-1$
+		return SEQUENCE_DIAGRAM;
 	}
 }
