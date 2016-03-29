d58dddf935fc2b7859313a82222a463a3b32cfa5
Frederic Bats <frederic.bats@obeo.fr>
2016-03-29 14:09:47 +0200

    Fix take selecton viewpoints into account

--- plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/dashboard/DashboardServices.java
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/dashboard/DashboardServices.java
@@ -18,6 +18,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.eclipse.uml2.uml.resource.UMLResource;
 
 import com.google.common.collect.Lists;
@@ -32,6 +33,31 @@ public class DashboardServices {
 	 * A singleton instance to be accessed by other java services.
 	 */
 	public static final DashboardServices INSTANCE = new DashboardServices();
+
+	/**
+	 * UMl-Designer Capture viewpoint name.
+	 */
+	public final String VP_CAPTURE = "Capture"; //$NON-NLS-1$
+
+	/**
+	 * UMl-Designer Design viewpoint name.
+	 */
+	public final String VP_DESIGN = "Design"; //$NON-NLS-1$
+
+	/**
+	 * UMl-Designer Extend viewpoint name.
+	 */
+	public final String VP_EXTEND = "Extend"; //$NON-NLS-1$
+
+	/**
+	 * UMl-Designer Reused viewpoint name.
+	 */
+	public final String VP_REUSED = "Reused"; //$NON-NLS-1$
+
+	/**
+	 * UMl-Designer Review viewpoint name.
+	 */
+	public final String VP_REVIEW = "Review"; //$NON-NLS-1$
 
 	/**
 	 * Hidden constructor.
@@ -63,5 +89,23 @@ public class DashboardServices {
 			}
 		}
 		return results;
+	}
+
+	/**
+	 * Check if viewpoint is enabled for the session.
+	 *
+	 * @param session
+	 *            sirius session
+	 * @param vpName
+	 *            name of viewpoint
+	 * @return true if enable
+	 */
+	public boolean isEnabledVP(Session session, String vpName) {
+		for (final Viewpoint vp : session.getSelectedViewpoints(false)) {
+			if (vp.getName().contains(vpName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/internal/testers/UMLViewpointEnablementPropertyTester.java
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/internal/testers/UMLViewpointEnablementPropertyTester.java
@@ -10,12 +10,15 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.uml2.design.internal.testers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.obeonetwork.dsl.uml2.design.dashboard.DashboardServices;
 
 /**
  * Tester to check viewpoints enablement.
@@ -23,15 +26,12 @@ import org.eclipse.sirius.viewpoint.description.Viewpoint;
  * @author Melanie Bats <a href="mailto:melanie.bats@obeo.fr">melanie.bats@obeo.fr</a>
  */
 public class UMLViewpointEnablementPropertyTester extends PropertyTester {
-	private final String vpCapture = "Capture"; //$NON-NLS-1$
 
-	private final String vpDesgin = "Desgin"; //$NON-NLS-1$
 
-	private final String vpReview = "Review"; //$NON-NLS-1$
-
-	private final String vpExtend = "Extend"; //$NON-NLS-1$
-
-	private final String vpReused = "Reused"; //$NON-NLS-1$
+	private final List<String> umlDesignerViewpoints = new ArrayList<String>(
+			Arrays.asList(DashboardServices.INSTANCE.VP_CAPTURE, DashboardServices.INSTANCE.VP_DESIGN,
+					DashboardServices.INSTANCE.VP_REVIEW, DashboardServices.INSTANCE.VP_EXTEND,
+					DashboardServices.INSTANCE.VP_REUSED));
 
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
 		final Collection<Session> sessions = SessionManager.INSTANCE.getSessions();
@@ -39,12 +39,8 @@ public class UMLViewpointEnablementPropertyTester extends PropertyTester {
 			// Check if an uml designer viewpoint is active for the current
 			// session
 			if (session != null) {
-				for (final Viewpoint vp : session.getSelectedViewpoints(false)) {
-					if (vp.getName().contains(vpCapture) || vp.getName().contains(vpDesgin)
-							|| vp.getName().contains(vpReview) || vp.getName().contains(vpExtend)
-							|| vp.getName().contains(vpReused)) {
-						return true;
-					}
+				for (final String vp : umlDesignerViewpoints) {
+					DashboardServices.INSTANCE.isEnabledVP(session, vp);
 				}
 			}
 		}
